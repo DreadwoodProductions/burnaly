2ad481d26146fe44c1f9cff87abaa4039f3f087d
DreadwoodProductions <dreadwoodproductions@icloud.com>
2025-01-27 00:53:06 -0600

    Reorganize file structure for Netlify

--- public/includes/config.php
+++
@@ -1,6 +0,0 @@
-<?php
-session_start();
-define('CLIENT_ID', '1333103933610135662');
-define('CLIENT_SECRET', 'NFJx68n_iwyWxuYNAwTX2ddF-To_RQS5');
-define('REDIRECT_URI', 'https://burnaly.com/callback.php');
-define('DISCORD_API', 'https://discord.com/api');
