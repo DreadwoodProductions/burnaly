a90cd43c9cf4409b721c6388852fccb3d18d59f5
DreadwoodProductions <dreadwoodproductions@icloud.com>
2025-01-27 00:18:56 -0600

    Initial commit

---
+++ public/includes/config.php
@@ -0,0 +1,6 @@
+<?php
+session_start();
+define('CLIENT_ID', '1333103933610135662');
+define('CLIENT_SECRET', 'NFJx68n_iwyWxuYNAwTX2ddF-To_RQS5');
+define('REDIRECT_URI', 'https://burnaly.com/callback.php');
+define('DISCORD_API', 'https://discord.com/api');
