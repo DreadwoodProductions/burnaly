ccdae983b3c06fc606da8228c8b4238fc7163b74
DreadwoodProductions <dreadwoodproductions@icloud.com>
2025-01-27 11:46:31 -0600

    Update getUserServers.js

--- netlify/functions/getUserServers/getUserServers.js
+++ netlify/functions/getUserServers/getUserServers.js
@@ -3,22 +3,19 @@ const fetch = require('node-fetch');
 exports.handler = async (event) => {
     const token = event.headers.authorization.split(' ')[1];
 
-    try {
-        const response = await fetch('https://discord.com/api/users/@me/guilds', {
-            headers: {
-                Authorization: `Bearer ${token}`
-            }
-        });
-        const serversData = await response.json();
+    const response = await fetch('https://discord.com/api/users/@me/guilds', {
+        headers: {
+            Authorization: `Bearer ${token}`
+        }
+    });
 
-        return {
-            statusCode: 200,
-            body: JSON.stringify(serversData)
-        };
-    } catch (error) {
-        return {
-            statusCode: 500,
-            body: JSON.stringify({ error: 'Failed to fetch user servers' })
-        };
-    }
+    const data = await response.json();
+
+    return {
+        statusCode: 200,
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+    };
 };
