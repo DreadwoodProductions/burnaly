0063e214c2ffbf642f177d7588ee41b76565bcd9
DreadwoodProductions <dreadwoodproductions@icloud.com>
2025-01-27 01:37:28 -0600

    Update function imports

--- netlify/functions/auth/auth.js
+++ netlify/functions/auth/auth.js
@@ -1,16 +1,12 @@
-const handler = async () => {
-    const clientId = '1333103933610135662';
-    const redirectUri = 'https://burnaly.com/.netlify/functions/callback';
-    const scopes = ['identify', 'guilds', 'email'];
-
-    const authUrl = `https://discord.com/oauth2/authorize?client_id=${clientId}&redirect_uri=${encodeURIComponent(redirectUri)}&response_type=code&scope=${scopes.join('+')}`;
+export const handler = async () => {
+    const clientId = process.env.DISCORD_CLIENT_ID;
+    const redirectUri = process.env.REDIRECT_URI;
+    const scope = 'identify guilds';
 
     return {
         statusCode: 302,
         headers: {
-            Location: authUrl,
+            Location: `https://discord.com/api/oauth2/authorize?client_id=${clientId}&redirect_uri=${redirectUri}&response_type=code&scope=${scope}`,
         },
     };
 };
-
-exports.handler = handler;  
