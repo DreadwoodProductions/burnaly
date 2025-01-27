dc935e624bfa3d93ada250c40583c22fd093cc31
DreadwoodProductions <dreadwoodproductions@icloud.com>
2025-01-27 11:46:08 -0600

    Update getUserProfile.js

--- netlify/functions/getUserProfile/getUserProfile.js
+++ netlify/functions/getUserProfile/getUserProfile.js
@@ -3,22 +3,19 @@ const fetch = require('node-fetch');
 exports.handler = async (event) => {
     const token = event.headers.authorization.split(' ')[1];
 
-    try {
-        const response = await fetch('https://discord.com/api/users/@me', {
-            headers: {
-                Authorization: `Bearer ${token}`
-            }
-        });
-        const userData = await response.json();
+    const response = await fetch('https://discord.com/api/users/@me', {
+        headers: {
+            Authorization: `Bearer ${token}`
+        }
+    });
 
-        return {
-            statusCode: 200,
-            body: JSON.stringify(userData)
-        };
-    } catch (error) {
-        return {
-            statusCode: 500,
-            body: JSON.stringify({ error: 'Failed to fetch user profile' })
-        };
-    }
+    const data = await response.json();
+
+    return {
+        statusCode: 200,
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+    };
 };
