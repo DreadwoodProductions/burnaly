22025fb7272826c6d6ae08c6e1800df9dbc0d1f9
DreadwoodProductions <dreadwoodproductions@icloud.com>
2025-01-30 12:22:55 -0600

    Update getUserServers.js

--- netlify/functions/getUserServers/getUserServers.js
+++ netlify/functions/getUserServers/getUserServers.js
@@ -1,23 +1,17 @@
-const fetch = require('node-fetch');
+# Retrieve User's Discord Servers
 
-exports.handler = async (event) => {
-    const token = event.headers.authorization.split(' ')[1];
+const axios = require('axios');
 
-    const response = await fetch('https://discord.com/api/users/@me/guilds', {
-        headers: {
-            Authorization: `Bearer ${token}`
-        }
-    });
-
-    const data = await response.json();
-
-    return {
-        statusCode: 200,
-        headers: {
-            'Content-Type': 'application/json',
-            'Access-Control-Allow-Origin': '*',
-            'Access-Control-Allow-Headers': 'Authorization, Content-Type'
-        },
-        body: JSON.stringify(data)
-    };
-};
+async function getUserDiscordServers(accessToken) {
+    try {
+        const response = await axios.get('https://discord.com/api/users/@me/guilds', {
+            headers: {
+                Authorization: `Bearer ${accessToken}`
+            }
+        });
+        return response.data;
+    } catch (error) {
+        console.error('Error retrieving Discord servers:', error);
+        throw error;
+    }
+}
