ff4b48e161d3d34e82c92445a5879a9e4a2816fa
DreadwoodProductions <dreadwoodproductions@icloud.com>
2025-02-19 20:51:33 -0600

    Update getGameDetails.js

--- netlify/functions/getGameDetails.js
+++ netlify/functions/getGameDetails.js
@@ -2,31 +2,12 @@ const fetch = require('node-fetch');
 
 exports.handler = async (event) => {
     try {
-        // Handle place details request
         if (event.path === '/.netlify/functions/getGameDetails') {
             const placeId = event.queryStringParameters.placeId;
             const response = await fetch(`https://games.roblox.com/v1/games/multiget-place-details?placeIds=${placeId}`, {
                 headers: {
-                    'Accept': 'application/json'
-                }
-            });
-            const data = await response.json();
-            return {
-                statusCode: 200,
-                headers: {
-                    'Content-Type': 'application/json',
-                    'Access-Control-Allow-Origin': '*'
-                },
-                body: JSON.stringify(data)
-            };
-        }
-        
-        // Handle thumbnail request
-        if (event.path === '/.netlify/functions/getGameDetails/thumbnail') {
-            const universeId = event.queryStringParameters.universeId;
-            const response = await fetch(`https://thumbnails.roblox.com/v1/games/multiget/thumbnails?universeIds=${universeId}&size=768x432&format=Png&isCircular=false`, {
-                headers: {
-                    'Accept': 'application/json'
+                    'Accept': 'application/json',
+                    'Cookie': `.ROBLOSECURITY=${process.env.ROBLOX_COOKIE}`
                 }
             });
             const data = await response.json();
