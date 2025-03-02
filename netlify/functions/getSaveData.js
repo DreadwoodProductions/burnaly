dbb97cf93aac16cc2feabc33f61a2db6f37b3cc1
DreadwoodProductions <dreadwoodproductions@icloud.com>
2025-03-02 12:55:17 -0600

    Update getSaveData.js

--- netlify/functions/getSaveData.js
+++ netlify/functions/getSaveData.js
@@ -1,1 +1,34 @@
+exports.handler = async (event) => {
+    const fs = require('fs').promises;
+    const path = require('path');
 
+    try {
+        const userId = event.queryStringParameters.userId;
+        const userDir = path.join('userData', userId.toString());
+        const files = await fs.readdir(userDir);
+        const saveData = [];
+
+        for (const file of files) {
+            const data = await fs.readFile(path.join(userDir, file), 'utf8');
+            saveData.push(JSON.parse(data));
+        }
+
+        return {
+            statusCode: 200,
+            headers: {
+                'Content-Type': 'application/json',
+                'Access-Control-Allow-Origin': '*'
+            },
+            body: JSON.stringify(saveData)
+        };
+    } catch (error) {
+        return {
+            statusCode: 500,
+            headers: {
+                'Content-Type': 'application/json',
+                'Access-Control-Allow-Origin': '*'
+            },
+            body: JSON.stringify({ error: error.message })
+        };
+    }
+};
