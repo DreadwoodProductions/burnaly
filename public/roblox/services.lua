5c9735d5e68f4121840f29dfdbe26002d7924ea2
DreadwoodProductions <dreadwoodproductions@icloud.com>
2025-03-10 12:27:03 -0500

    Create services.lua

---
+++ public/roblox/services.lua
@@ -0,0 +1,10 @@
+return {
+    Players = cloneref(game:GetService("Players")),
+    ReplicatedStorage = cloneref(game:GetService("ReplicatedStorage")),
+    VirtualUser = cloneref(game:GetService("VirtualUser")),
+    RunService = cloneref(game:GetService("RunService")),
+    HttpService = cloneref(game:GetService("HttpService")),
+    TweenService = cloneref(game:GetService("TweenService")),
+    UserInputService = cloneref(game:GetService("UserInputService")),
+    Workspace = cloneref(workspace)
+}
