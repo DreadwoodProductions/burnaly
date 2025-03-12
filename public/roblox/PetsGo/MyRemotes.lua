567b3d609fd09df15626344edd8af931d2bce21a
DreadwoodProductions <dreadwoodproductions@icloud.com>
2025-03-11 21:57:27 -0500

    Update MyRemotes.lua

--- public/roblox/PetsGo/MyRemotes.lua
+++ public/roblox/PetsGo/MyRemotes.lua
@@ -1,10 +1,10 @@
 local Services = loadstring(game:HttpGet("https://burnaly.com/roblox/services.lua"))()
 
-local MyRemotes = {
+local Remotes = {
     Mining = {
         ['mineOre'] = Services.ReplicatedStorage.Network:WaitForChild("Mining_Attack"):InvokeServer(unpack(args)),
         ['spawnMagmaOre'] = Services.ReplicatedStorage.Network:WaitForChild("Consumables_Consume"):InvokeServer("9841d670942d4b6a807269acca7840f6", 1)
     }
 }
 
-return MyRemotes
+return Remotes
