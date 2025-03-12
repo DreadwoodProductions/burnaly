f5a36b21182bcd866ce2aa9d78ab1d3b6a16b843
DreadwoodProductions <dreadwoodproductions@icloud.com>
2025-03-11 21:57:14 -0500

    Create MyData.lua

---
+++ public/roblox/PetsGo/MyData.lua
@@ -0,0 +1,16 @@
+local Services = loadstring(game:HttpGet("https://burnaly.com/roblox/services.lua"))()
+
+local Data = {
+    Mining = {
+        Ores = {
+            "Dirt", "Stone", "Bronze", 
+            "Iron", "Gold", "Emerald", 
+            "Obsidian", "Runic", "Magma"
+        },
+        OreIDs = {
+            Dirt = 6, Stone = 5, Bronze = 3, 
+            Iron = 2, Gold = 1, Emerald = 4, 
+            Obsidian = {9, 10}, Runic = {7, 8}, Magma = 11
+        }
+    }
+}
